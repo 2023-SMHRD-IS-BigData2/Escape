63887aaf102e41d11f518e711534f1f44e91a25c
smhrd <smhrd@211.228.36.173>
2023-09-22 16:25:41 +0900

    정윤오 퀴즈 메소드 내 퀴즈 못 풀 경우 무한루프 빠지게 설계 탈출 키워드 생성

--- EscRoom/src/Controller/QuizChoice.java
+++ EscRoom/src/Controller/QuizChoice.java
@@ -7,21 +7,23 @@ import java.util.Scanner;
 public class QuizChoice {
 	Scanner sc = new Scanner(System.in);
 	Random rd = new Random();
-	int n;
+	boolean isAns = false; //정답에 따라 변경되어 while 탈출
 	ArrayList<String> QuizList = new ArrayList<>(); // 퀴즈db에서 저장받을 배열
 
 	public void Quiz(int roomHint) {
-		while (true) {
+		while (!isAns) {
 			// 첫번째 힌트 받고 퀴즈 출력
-			System.out.println("퀴즈 출력"); //메소드 작동 테스트용
-			break; //테스트 중
+			System.out.println("퀴즈 출력"); // 메소드 작동 테스트용
+			break; // 테스트 중
 //			System.out.println(QuizList.get(i).퀴즈); //퀴즈 불러오기
 //			String answer = sc.next();
 //			if (answer.equals(QuizList.get(i).정답)) {
 //				QuizList.remove(i); //맞힌 퀴즈는 삭제
-//				break;
-//			} else {
+//				isAns = true;
+//			} else if{
 //				System.out.println("다시 생각해"); // 틀릴 경우 재시도
+//			} else if(answer.quals("살려주세요")) { //무한루프 탈출 키워드
+//				isAns = true;
 //			}
 		}
 
