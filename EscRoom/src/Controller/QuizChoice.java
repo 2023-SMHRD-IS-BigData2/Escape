4652a5c1b0db26b429e2deb7230b22d1b0845333
smhrd <smhrd@211.228.36.173>
2023-09-22 19:04:21 +0900

    정윤오 퀴즈 클래스 수정

--- EscRoom/src/Controller/QuizChoice.java
+++ EscRoom/src/Controller/QuizChoice.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import Model.QuizDAO;
+
 public class QuizChoice {
 	Scanner sc = new Scanner(System.in);
 	Random rd = new Random();
+	QuizDAO quiz = new QuizDAO();
 	boolean isAns = false; //정답에 따라 변경되어 while 탈출
-	ArrayList<String> QuizList = new ArrayList<>(); // 퀴즈db에서 저장받을 배열
+//	ArrayList<String> QuizList = new ArrayList<>(); // 퀴즈db에서 저장받을 배열
 
 	public void Quiz(int roomHint) {
+		int n = rd.nextInt();
 		while (!isAns) {
-			//힌트 선택되면 퀴즈 출력
-			System.out.println("퀴즈 출력"); // 메소드 작동 테스트용
-			break; // 테스트 중
-//			System.out.println(QuizList.get(i).퀴즈); //퀴즈 불러오기
-//			String answer = sc.next();
-//			if (answer.equals(QuizList.get(i).정답)) {
-//				QuizList.remove(i); //맞힌 퀴즈는 삭제
-//				isAns = true;
-//			} else if{
-//				System.out.println("다시 생각해"); // 틀릴 경우 재시도
-//			} else if(answer.quals("살려주세요")) { //무한루프 탈출 키워드
-//				isAns = true;
-//			}
-		}
+			System.out.println(quiz.selectAll(n).getQuiz()); //퀴즈 불러오기
+			String answer = sc.next();
+			if (answer.equals(quiz.selectAll(n).getAnswer())) {
+				
+			} else if{
+				System.out.println("다시 생각해"); // 틀릴 경우 재시도
+			} else if(answer.equals("살려주세요")) { //무한루프 탈출 키워드
+				isAns = true;
+			}//else if
+		}//while
 
 	}
 
