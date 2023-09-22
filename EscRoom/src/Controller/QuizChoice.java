cc8ad9f07cdd208a35a81e104cd7a46f8a6eabc8
smhrd <smhrd@211.228.36.173>
2023-09-22 14:34:31 +0900

    정윤오 컨트롤러 패키지에 퀴즈선택 클래스 생성

---
+++ EscRoom/src/Controller/QuizChoice.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class QuizChoice {
+	Scanner sc = new Scanner(System.in);
+	Random rd = new Random();
+	int n;
+	ArrayList<String> QuizList = new ArrayList<>(); // 퀴즈db에서 저장받을 배열
+
+	public void Quiz(int roomHint) {
+		while (true) {
+			// 첫번째 힌트 받고 퀴즈 출력
+//			System.out.println(QuizList.get(i).퀴즈);
+//			String answer = sc.next();
+//			if (answer.equals(QuizList.get(i).정답)) {
+//				QuizList.remove(i);
+//				break;
+//			} else {
+//				System.out.println("다시 생각해"); // 틀릴 경우 재시도
+//			}
+		}
+
+	}
+
+}
