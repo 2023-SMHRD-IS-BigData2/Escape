6e647a145ba23b86ca7d94c6a6d68d73a84633d6
user <user@220.80.88.122>
2023-09-25 15:24:21 +0900

    정찬호 - 상대경로 및 출력 수정

--- EscRoom/src/Controller/QuizChoice.java
+++ EscRoom/src/Controller/QuizChoice.java
@@ -23,12 +23,12 @@ public class QuizChoice {
 
 	public void Quiz(int cnt, int roomHint) {
 		ArrayList<MusicVO> playlist = new ArrayList<>();
-		playlist.add(new MusicVO("시작브금1", "BGM", "BGM\\방탈출BGM.mp3"));
-		playlist.add(new MusicVO("시작브금2", "BGM", "BGM\\011_키보드+타이핑.mp3"));
-		playlist.add(new MusicVO("시작브금3", "BGM", "BGM\\효과음3.mp3"));
-		playlist.add(new MusicVO("퀴즈출제", "BGM", "BGM\\11멜로디.mp3"));
-		playlist.add(new MusicVO("퀴즈정답", "BGM", "BGM\\015_띠링.mp3"));
-		playlist.add(new MusicVO("퀴즈오답", "BGM", "BGM\\107_왕왕왕와~~~~~.mp3"));
+		playlist.add(new MusicVO("시작브금1", "BGM", "BGM/방탈출BGM.mp3"));
+		playlist.add(new MusicVO("시작브금2", "BGM", "BGM/011_키보드+타이핑.mp3"));
+		playlist.add(new MusicVO("시작브금3", "BGM", "BGM/효과음3.mp3"));
+		playlist.add(new MusicVO("퀴즈출제", "BGM", "BGM/11멜로디.mp3"));
+		playlist.add(new MusicVO("퀴즈정답", "BGM", "BGM/015_띠링.mp3"));
+		playlist.add(new MusicVO("퀴즈오답", "BGM", "BGM/1107_왕왕왕와~~~~~.mp3"));
 		MusicPlayer bgm = new MusicPlayer(playlist);
 		if (roomHint == 9 || roomHint == 14) {
 			int n = rd.nextInt(20) + 1;
